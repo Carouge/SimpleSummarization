265bb37ceac0522e10b25e9e9a26810094b9270f
programminggirl <dobko_m@ucu.edu.ua>
2018-06-05 20:58:52 +0300

    added BLEU metric

--- summarize_textrank.py
+++ summarize_textrank.py
@@ -1,6 +1,7 @@
 from gensim.summarization.summarizer import summarize
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+from nltk.translate.bleu_score import sentence_bleu
 
 
 with open('./document.txt', "r") as f:
@@ -13,4 +14,9 @@ filtered_words = [word for word in words if word not in stopwords.words('english
 filtered_words = tokenizer.tokenize(' '.join(filtered_words))
 
 result = ' '.join(filtered_words)
-print(result)
+reference = [['abstractive','scientific', 'text', 'summarization', 'using', 'GAN']]
+candidate = filtered_words
+print(filtered_words)
+score = sentence_bleu(reference, candidate)
+
+print(score)
